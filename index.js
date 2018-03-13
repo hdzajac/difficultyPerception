9f8211d6f47cb6b995c81b353c2b1ca3e3dee4c0
hubz00 <hubert.zajac@outlook.com>
2018-03-13 23:40:06 +0100

    skeeton

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ app.get('/',function(req,res){
     //__dirname : It will resolve to your project folder.
 });
 
+app.get('/form',function(req,res){
+    res.sendFile(path.join(__dirname+'/pages/form.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+app.get('/game',function(req,res){
+    res.sendFile(path.join(__dirname+'/pages/game.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+app.get('/question',function(req,res){
+    res.sendFile(path.join(__dirname+'/pages/question.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+app.get('/finish',function(req,res){
+    res.sendFile(path.join(__dirname+'/pages/finish.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+
 app.listen(3000);
 
 console.log("Running at Port 3000");
